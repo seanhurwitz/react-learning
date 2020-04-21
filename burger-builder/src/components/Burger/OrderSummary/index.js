a51749cbd28cef2738b4ae30f1be44672afd6df5
seanhurwitz <57170877+seanhurwitz@users.noreply.github.com>
2020-04-21 14:46:37 +0200

    shouldcomponentupdate for modal efficiency

--- burger-builder/src/components/Burger/OrderSummary/index.js
+++ burger-builder/src/components/Burger/OrderSummary/index.js
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Button } from '../../UI';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((ingredient) => (
-    <li key={ingredient}>
-      <span style={{ textTransform: 'capitalize' }}>{ingredient}</span>:{' '}
-      {props.ingredients[ingredient]}
-    </li>
-  ));
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        Total Price: <strong>${props.totalPrice.toFixed(2)}</strong>
-      </p>
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[Order Summary Will Update]');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (ingredient) => (
+        <li key={ingredient}>
+          <span style={{ textTransform: 'capitalize' }}>{ingredient}</span>:{' '}
+          {this.props.ingredients[ingredient]}
+        </li>
+      ),
+    );
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          Total Price: <strong>${this.props.totalPrice.toFixed(2)}</strong>
+        </p>
 
-      <Button buttonType="Danger" clicked={props.cancel}>
-        KEEP BUILDING
-      </Button>
-      <Button buttonType="Success" clicked={props.continue}>
-        CHECKOUT
-      </Button>
-    </Fragment>
-  );
-};
+        <Button buttonType="Danger" clicked={this.props.cancel}>
+          KEEP BUILDING
+        </Button>
+        <Button buttonType="Success" clicked={this.props.continue}>
+          CHECKOUT
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- burger-builder/src/components/UI/Modal/index.js
+++ burger-builder/src/components/UI/Modal/index.js
@@ -1,20 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop';
 
-const modal = (props) => (
-  <Fragment>
-    <Backdrop show={props.show} clicked={props.clicked} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-200vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentDidUpdate() {
+    console.log('[Modal Will Update]');
+  }
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.clicked} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-200vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
